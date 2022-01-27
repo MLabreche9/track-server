5e4e0bee1ee2f9832ca4afa471c3e13b668fe51f
Meghan La Breche <Megavictoria@gmail.com>
2022-01-26 19:49:09 -0800

    signup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
+
+const mongoUri =
+	'mongodb+srv://admin:oasswordpassword@cluster0.cu8ic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+	console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hi there!');
